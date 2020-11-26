6f0ece3af210c74d5b68ea3850c9ff997680ed1e
nolastemgarden <nolastemgarden@gmail.com>
2020-11-25 20:21:56 -0600

    Update MathGames.js
    Convert to black bg and tidy up.

--- src/pages/MathGames.js
+++ src/pages/MathGames.js
@@ -1,17 +1,11 @@
 import React from 'react';
 
 // MY components
-// import LandingHeader from "../components/LandingHeader";
-// import MobileLandingCards from "../components/LandingCards";
-// import DesktopLandingCards from "../components/DesktopLandingCards";
 import Navbar from "../components/Navbar/Navbar";
+import MathGamesCards from "../components/MathGamesCards";
 
 // MY images
-// import bananas_and_loquat from "../images/bananas_and_loquat.jpg";
-// import logo from "../images/incomplete_logo.png";
 import leaves from "../images/leaves_cropped_for_mobile.jpg";
-// import leaves from "../images/leaves_edited.jpg";
-// import turmeric from "../images/turmeric.jpg";
 
 // MUI Imports
 import Container from '@material-ui/core/Container';
@@ -22,222 +16,92 @@ import { makeStyles } from '@material-ui/core/styles';
 import { Typography } from '@material-ui/core';
 const useStyles = makeStyles((theme) => ({
     root: {
-        height: '100vh',
         width: '100vw',
-        // minHeight: '100vh',
-        // backgroundColor: theme.palette.common.white,
-
-    },
-    mobileBody: {
-        // border: 'solid red 1px',
-        // height: 'calc(100% - 3.5rem)',   // 3.5rem is the height of the mobileNavbar 
         height: 'auto',
-        width: '100%',
         minHeight: '100vh',
-        backgroundImage: `
-            linear-gradient(90deg, 
-                rgba(58,94,22,0.70) 0%, 
-                rgba(58,94,22,0.80) 100%),
-            url(${leaves})
-        `,
-        backgroundPosition: 'center top',
-        backgroundRepeat: 'repeat-y',
-        backgroundSize: 'contain',
+        backgroundColor: theme.palette.common.black,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
     },
-    desktopBody: {
+    container: {
+        // border: 'solid green 2px',
+        minHeight: '100vh',
         height: 'auto',
-        width: '100%',
-        minHeight: '100vh',
-        backgroundImage: `
-            linear-gradient(90deg, 
-                rgba(58,94,22,0.70) 0%, 
-                rgba(58,94,22,0.80) 100%),
-            url(${leaves})
-        `,
-        backgroundPosition: 'center top',
-        backgroundRepeat: 'repeat-y',
-        backgroundSize: 'cover',
+        backgroundColor: 'inherit',
+        display: 'flex',
+        flexDirection: 'column',
+        // justifyContent: 'center',
+        alignItems: 'center'
     },
-    desktopContainer: {
-        backgroundImage: `
-            linear-gradient(90deg, 
-                rgba(58,94,22,0.00) 0%, 
-                rgba(58,94,22,0.50) 5%,
-                rgba(58,94,22,0.70) 10%, 
-                rgba(58,94,22,0.70) 90%,
-                rgba(58,94,22,0.50) 95%,
-                rgba(58,94,22,0.00) 100%)
-        `,
-        backgroundPosition: 'center top',
-        // yOffset: '12rem',
-        backgroundRepeat: 'repeat-y',
-        backgroundSize: 'cover',
-    },
-
-
-    mobileTitleText: {
+    titleText: {
         color: theme.palette.common.white,
-        // maxWidth: '90%',
-        padding: '2.5rem',
-        paddingTop: '7.0rem',
-        paddingBottom: '3.0rem',
-        textShadow: '0.15rem 0.15rem 0.3rem black',
-        // fontSize: 'calc(1rem + 5vw)',
-        fontSize: '7vw'
+        padding: '4.0rem',
     },
-    mobileSubtitleText: {
-        // border: 'solid red 1px',
+    subtitleText: {
         color: theme.palette.common.white,
-        padding: '1rem 3rem 1rem 3rem',
-        textShadow: '0.12rem 0.12rem 0.20rem black',
+        padding: '2.0rem 4.0rem',
     },
-
-    desktopTitleText: {
+    header: {
         color: theme.palette.common.white,
-        padding: '2.5rem',
-        paddingTop: '12.0rem',
-        paddingBottom: '3.0rem',
-        textShadow: '0.15rem 0.15rem 0.3rem black',
+        padding: '0.0rem 0.0rem',
     },
-    desktopSubtitleText: {
-        // border: 'solid red 1px',
-        color: theme.palette.common.white,
-        padding: '1rem 5rem 2rem 5rem',
-        // padding: '5rem',
-        textShadow: '0.12rem 0.12rem 0.20rem black',
-    },
-
-    landingHeader: {
-        width: '100%',
-        height: '3rem',
-    },
+    // body: {
+    //     color: theme.palette.common.white,
+    //     padding: '0.0rem 0.0rem',
+    //     paddingBottom: '1.5rem',
+    //     // maxWidth: '640px'
+    // },
+    
     cardArea: {
-        // border: 'solid red 1px',
-        height: 'auto',
-        width: '100%',
-        padding: '1rem'
-
+        padding: '2.0rem',
     },
+
 }));
 
 
-// LandingPage is responsible for rendering a Navbar and either the Mobile or Desktop version
-// depending on screen width. The Navbar contains its own M.vs.D. rendering logic. 
-export default function LandingPage(props) {
-    const classes = useStyles();
-    return (
-        <Box className={classes.root}  >
-            <Navbar />
-            <MobileBody />
-            <DesktopBody />
-        </Box>
-    )
+export default function MathGamesPage(props) {
+const classes = useStyles();
+return (
+    <Box className={classes.root}  >
+        <Navbar />
+        <PageBody />
+    </Box>
+)
 }
 
-function MobileBody(props) {
+function PageBody(props) {
     const classes = useStyles();
     return (
-        <Box
-            className={classes.mobileBody}
-            display={{ xs: 'block', lg: 'none' }}
-        >
-            <Typography
-                className={classes.mobileTitleText}
-                align='center'
-                color='textPrimary'
-                component='h1'
-                variant='h2'
-            >
-                COPIED THE LANDING PAGE
+        <Container className={classes.pageBody} maxWidth='md' >
+            <Typography className={classes.titleText} align='center' color='textPrimary' component='h1' variant='h1'>
+                Math Games
             </Typography>
+            <Box  p={4} >
+                <Typography className={classes.body} align='justify' color='secondary' component='h2' variant='body1' >
+                    I love teaching mathematical thinking through the lens of simple games.  
+                    Both games of strategy and games of chance have something to teach us.
+                    Mathem
 
-            <Box
-                className={classes.xsText}
-                display={{ xs: 'block', sm: 'none' }}
-            >
-                <Typography
-                    className={classes.mobileSubtitleText}
-                    align='justify'
-                    color='textPrimary'
-                    component='h2'
-                    variant='body1'
-                >
-                    COPIED THE LANDING PAGE
+
+                    Proof is the heart of all mathematical thinking; we start with a few simple assumptions (axioms) 
+                    and we put them together in creative ways to prove more complex and interesting theorems. 
+                    But what does it really mean to "prove" something is true?
+                    That is a question philosophers and mathematicians have struggled with for a long time. 
+                    It's deeper than just showing your work or explaining your reasoning. 
+                    Mathematical proof is subtle and difficult explain, yet a game as simple as tic-tac-toe offers us 
+                    an excellent gateway to understanding this elusive concept. 
                 </Typography>
+                
             </Box>
-
-            <Box
-                className={classes.smText}
-                display={{ xs: 'none', sm: 'block' }}
-            >
-                <Typography
-                    className={classes.mobileSubtitleText}
-                    align='justify'
-                    color='textPrimary'
-                    component='h2'
-                    variant='body1'
-                >
-                    COPIED THE LANDING PAGE
-            </Typography>
+            <Box className={classes.cardArea} >
+                <MathGamesCards />
             </Box>
-
-            <Box
-                className={classes.cardArea}
-                display={{ xs: 'block' }}
-            >
-                {/* <MobileLandingCards /> */}
-            </Box>
-        </Box>
+        </Container>
     )
 }
 
 
-function DesktopBody(props) {
-    const classes = useStyles();
-    return (
-        <Box
-            className={classes.desktopBody}
-            display={{ xs: 'none', lg: 'block' }}
-        >
-            <Container
-                className={classes.desktopContainer}
-                maxWidth='md'
-                disableGutters
-            >
-                <Typography
-                    className={classes.desktopTitleText}
-                    align='center'
-                    color='textPrimary'
-                    component='h1'
-                    variant='h2'
-                >
-                    COPIED THE LANDING PAGE
-                </Typography>
 
-                <Box
-                    className={classes.smText}
-                    display={{ xs: 'none', sm: 'block' }}
-                >
-                    <Typography
-                        className={classes.desktopSubtitleText}
-                        align='justify'
-                        color='textPrimary'
-                        component='h2'
-                        variant='h5'
-                    >
-                        COPIED THE LANDING PAGE
-                    </Typography>
-                </Box>
 
-                <Box
-                    className={classes.cardArea}
-                    display={{ xs: 'block' }}
-                >
-                    {/* <MobileLandingCards /> */}
-                </Box>
-            </Container>
-        </Box>
-
-    )
-}
 
