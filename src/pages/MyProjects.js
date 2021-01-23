580580b06cc0ad4f937f0ccca0eb6cc9bbf4ff2f
nolastemgarden <nolastemgarden@gmail.com>
2021-01-22 18:02:28 -0600

    Delete unused import

--- src/pages/MyProjects.js
+++ src/pages/MyProjects.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 // MY components
 import Navbar from "../components/Navbar/Navbar";
-import MyProjectsHeader from "../components/MyProjectsHeader";
 import ProjectCards from "../components/ProjectCards";
 
 // MY images
